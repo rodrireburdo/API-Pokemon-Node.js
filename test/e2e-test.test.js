7300c4b9b6947d8754ec10ac62246e5950085a63
Rodri Reburdo <rodri.reburdo01@gmail.com>
2025-01-02 23:15:44 -0300

    first commit

---
+++ test/e2e-test.test.js
@@ -0,0 +1,20 @@
+import { assert } from 'chai';
+import { app } from '../app.js';
+import { use } from 'chai';
+import superagent from 'chai-superagent';
+import request from 'supertest';
+
+use(superagent());
+
+describe('Suite de prueba e2e', () => {
+    it('should return hello world', (done) => {
+        request(app)
+            .get('/')
+            .end((err, res) => {
+                console.log('A');
+                assert.equal(res.text, "Hello world!")
+                done();
+            });
+        console.log('B');
+    });
+});
---
+++ test/uni-test.test.js
@@ -0,0 +1,11 @@
+// const { describe, it } = require('mocha');
+// const assert = require('chai').assert;
+import { assert } from 'chai';
+
+
+describe('Suite de prueba', () => {
+    it('should return 4', () => {
+        let va = 4;
+        assert.equal(va, 4);
+    });
+});
